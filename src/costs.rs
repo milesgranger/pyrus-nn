5c7e321a616a232686e048ea7d2caed5b20eb613
Miles Granger <miles59923@gmail.com>
2019-02-17 13:14:17 +0100

    Implement accuracy score

--- src/costs.rs
+++ src/costs.rs
@@ -1,5 +1,33 @@
 use ndarray::{ArrayView2, Zip};
+use ndarray_parallel::prelude::*;
 
+/// Cost function selection `enum`
+pub enum CostFunc {
+    MSE,
+    MAE,
+    Accuracy
+}
+
+impl std::default::Default for CostFunc {
+    fn default() -> Self { CostFunc::MSE }
+}
+
+/// Measure accuracy score
+pub fn accuracy_score(y_true: ArrayView2<f32>, y_hat: ArrayView2<f32>) -> f32 {
+    y_true.iter()
+        .zip(y_hat.iter())
+        .map(|(yt, yh)| accuracy(*yt, *yh))
+        .sum::<f32>() / y_true.rows() as f32
+}
+
+/// Measure if two `f32` elements are equal; here for consistency.
+pub fn accuracy(y_true: f32, y_hat: f32) -> f32 {
+    if y_hat == y_true {
+        1.
+    } else {
+        0.
+    }
+}
 
 /// Determine the Mean Squared Error
 pub fn mean_squared_error(y_true: ArrayView2<f32>, y_hat: ArrayView2<f32>) -> f32 {
@@ -25,14 +53,4 @@ pub fn mean_absolute_error(y_true: ArrayView2<f32>, y_hat: ArrayView2<f32>) -> f
 /// Absolute error between two `f32` values
 pub fn absolute_error(y_true: f32, y_hat: f32) -> f32 {
     (y_true - y_hat).abs()
-}
-
-/// Cost function selection `enum`
-pub enum CostFunc {
-    MSE,
-    MAE,
-}
-
-impl std::default::Default for CostFunc {
-    fn default() -> Self { CostFunc::MSE }
 }
