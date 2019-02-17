8270e138645b55546c90c5edae186ee4f27dc4f4
Miles Granger <miles59923@gmail.com>
2019-02-17 14:34:46 +0100

    Implement cross entropy loss

--- src/costs.rs
+++ src/costs.rs
@@ -1,15 +1,43 @@
-use ndarray::{ArrayView2, Zip};
+use ndarray::{ArrayView2, Zip, Axis};
 use ndarray_parallel::prelude::*;
 
 /// Cost function selection `enum`
 pub enum CostFunc {
     MSE,
     MAE,
-    Accuracy
+    Accuracy,
+    CrossEntropy
 }
 
 impl std::default::Default for CostFunc {
     fn default() -> Self { CostFunc::MSE }
+}
+
+/// Cross entropy; aka logistic loss / log loss
+pub fn cross_entropy(y_true: ArrayView2<f32>, y_hat: ArrayView2<f32>) -> f32 {
+
+    let elipson = 1e-15;  // TODO: Make this a parameter
+
+    // Clip
+    let mut y_hat = y_hat.mapv(|v| if v > 1.0 { v - elipson } else { v + elipson }).to_owned();
+
+    // Normalize
+    y_hat = y_hat.to_owned() / y_hat.sum_axis(Axis(1)).into_shape((y_hat.shape()[0], 1)).unwrap().to_owned();
+
+    // Loss
+    -(y_true.to_owned() * y_hat.mapv(|v| v.ln())).sum_axis(Axis(1)).sum() / y_hat.rows() as f32
+}
+
+/// Cross entropy score of single element
+pub fn single_cross_entropy(y_true: f32, y_hat: f32) -> f32 {
+
+    let y_hat = if y_hat > 1.0 { y_hat - 1e-15 } else { y_hat + 1e-15 };
+
+    if y_true as usize == 1 {
+        -(y_hat.ln())
+    } else {
+        -((1. - y_hat).ln())
+    }
 }
 
 /// Measure accuracy score
